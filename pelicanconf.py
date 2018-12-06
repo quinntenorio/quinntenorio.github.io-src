fbc6f79b2ed9e728eaed803c99b4f34b056ee417
Quinn Tenorio <quinnktenorio@gmail.com>
2018-12-05 19:06:22 -0700

    Baby steps, setting up.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,18 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Quinn Tenorio'
 SITENAME = 'Quinn Tenorio'
-SITEURL = ''
+SITETITLE = 'Quinn Tenorio'
+SITESUBTITLE = 'Portfolio'
+SITELOGO = '/images/quinn.jpg'
+INDEX_SAVE_AS = 'blog.html'
 
 PATH = 'content'
 
 TIMEZONE = 'America/Denver'
 
 DEFAULT_LANG = 'en'
+
+STATIC_PATHS = ["images", "pages"]
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,16 +25,21 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('about', 'http://getpelican.com/'),
+         ('contact', 'http://python.org/'),
+         ('resume', 'http://jinja.pocoo.org/'),
+         ('projects', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/qktenorio'),
+          ('github', 'http://github.com/quinntenorio'),
+          ('instagram', 'http://instagram.com/quinntenorio'),
+          ('facebook', 'https://www.facebook.com/quinnktenorio')
+          )
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = "../pelican-themes/Flex"
